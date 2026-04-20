528d4fe7208e0faa4dd29024d6ccd3e9bd78e4e8
Jorge Sebastian Otalora Bernal <js.otalorab12@uniandes.edu.co>
2026-04-19 22:07:26 -0500

    Creacion de Data.ts

---
+++ data.ts
@@ -0,0 +1,58 @@
+import { Serie } from "./Serie.js";
+
+export const series = [
+  new Serie(
+    1,
+    "Breaking Bad",
+    "AMC",
+    5,
+    "A high school chemistry teacher turned methamphetamine producer navigates the dangers of the criminal world.",
+    "https://www.amc.com/shows/breaking-bad",
+    "https://m.media-amazon.com/images/I/81m+d6YwYyL._AC_SY679_.jpg"
+  ),
+  new Serie(
+    2,
+    "Orange Is the New Black",
+    "Netflix",
+    6,
+    "A woman is sent to prison and has to adapt to life behind bars.",
+    "https://www.netflix.com/co/title/70242311",
+    "https://m.media-amazon.com/images/I/91M1H7xwSJL._AC_SY679_.jpg"
+  ),
+  new Serie(
+    3,
+    "Game of Thrones",
+    "HBO",
+    7,
+    "Noble families fight for control of the Iron Throne in the land of Westeros.",
+    "https://www.hbo.com/game-of-thrones",
+    "https://m.media-amazon.com/images/I/91dSMhdIzTL._AC_SY679_.jpg"
+  ),
+  new Serie(
+    4,
+    "The Big Bang Theory",
+    "CBS",
+    12,
+    "A group of socially awkward scientists navigate work, friendship, and love.",
+    "https://www.cbs.com/shows/big_bang_theory/",
+    "https://m.media-amazon.com/images/I/81kRk6wN5rL._AC_SY679_.jpg"
+  ),
+  new Serie(
+    5,
+    "Sherlock",
+    "BBC",
+    4,
+    "A modern update finds the famous detective solving crime in 21st century London.",
+    "https://www.bbc.co.uk/programmes/b018ttws",
+    "https://m.media-amazon.com/images/I/71tbalAHYCL._AC_SY679_.jpg"
+  ),
+  new Serie(
+    6,
+    "A Very English Scandal",
+    "BBC",
+    2,
+    "The true story of a British politician accused of conspiracy and attempted murder.",
+    "https://www.bbc.co.uk/programmes/p065smy4",
+    "https://m.media-amazon.com/images/I/91Uq0jvX8BL._AC_SY679_.jpg"
+  )
+];
